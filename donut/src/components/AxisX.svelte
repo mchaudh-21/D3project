1f3a9b98497c24c8f00bfe2d6badb63a61c7720e
Manasi Chaudhary <mchaudh@ad.unc.edu>
2024-06-14 15:25:11 -0400

    new

---
+++ donut/src/components/AxisX.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let xScale, groupByOrg; // y axis can be organized by continent or the color groupings   
+    $: ticks = xScale.domain();
+    import {fade} from "svelte/transition"
+</script>
+
+<g class = "axis x">
+    {#each ticks as tick, index}  <!-- loop through ticks -->
+        {#if groupByOrg}
+            <g class="tick" in:fade={{delay: index*100}} out:fade={{duration:200}}> 
+                <text x={xScale(tick)}> CC: {tick} </text>
+            </g>
+        {/if}
+    {/each}
+</g>
---
+++ donut/src/components/AxisY.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let yScale, width, height;
+    $: ticks = yScale.ticks(12); //returns array of values from scale's domain
+</script>
+
+<!-- <g> tag is used to group other SVG elements-->
+<g class = "axis y">
+    {#each ticks as tick}  <!-- loop through ticks -->
+        <g class = "tick" transform = "translate({yScale(tick)},0)"> 
+            <text x = "5" y = {height}> 
+                {tick/100}  
+            </text>
+            <line 
+                x1 = "0" 
+                x2 = "0"
+                y1 = "0"
+                y2 = {height}
+                stroke = "hsla(289, 75%, 9% 1)" />
+        </g>
+    {/each}
+    <text class = "axis-title" 
+        x = {width - innerWidth/2 + 100} 
+        y = {height}
+        dy = 15
+        dominant-baseline = "hanging"
+        text-anchor = "end"
+        >
+        
+        Cost in Dollars
+
+    </text>
+</g>
